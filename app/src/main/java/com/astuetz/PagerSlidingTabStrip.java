96374728a27c3d923dd042002efc0278af6556b5
Taylor Jean-Jacques <tjeanjacques@fb.com>
2017-07-05 17:48:17 -0700

    mentions bar back

--- app/src/main/java/com/astuetz/PagerSlidingTabStrip.java
+++ app/src/main/java/com/astuetz/PagerSlidingTabStrip.java
@@ -1,54 +1,50 @@
 package com.astuetz;
 
 import android.annotation.SuppressLint;
-
-/*
- * Totally borrowing this version of the PagerSlidingTabStrip that supports custom view
- * https://github.com/astuetz/PagerSlidingTabStrip/pull/101
- *
- *
- * Copyright (C) 2013 Andreas Stuetz <andreas.stuetz@gmail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-        import java.util.Locale;
-
-        import android.annotation.SuppressLint;
-        import android.content.Context;
-        import android.content.res.TypedArray;
-        import android.graphics.Canvas;
-        import android.graphics.Paint;
-        import android.graphics.Paint.Style;
-        import android.graphics.Typeface;
-        import android.os.Build;
-        import android.os.Parcel;
-        import android.os.Parcelable;
-        import android.support.v4.view.ViewPager;
-        import android.support.v4.view.ViewPager.OnPageChangeListener;
-        import android.util.AttributeSet;
-        import android.util.DisplayMetrics;
-        import android.util.TypedValue;
-        import android.view.Gravity;
-        import android.view.View;
-        import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-        import android.widget.HorizontalScrollView;
-        import android.widget.ImageButton;
-        import android.widget.LinearLayout;
-        import android.widget.TextView;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.codepath.apps.twitter.R;
+
+import java.util.Locale;
+
+//Borrowing PagerSlidingTabStrip from Andreas Stuetz
+//  https://github.com/astuetz/PagerSlidingTabStrip/pull/101
+//
+//
+//  Copyright (C) 2013 Andreas Stuetz <andreas.stuetz@gmail.com>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 @SuppressLint("NewApi")
 public class PagerSlidingTabStrip extends HorizontalScrollView {
